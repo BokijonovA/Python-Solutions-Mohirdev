184fb2859946726894b7ae66218dce62f25b740f
Asliddin Bokijonov <asliddinbokijonov@gmail.com>
2023-05-08 21:47:16 +0200

    7th day is added.

---
+++ 7th day nesting/nesting.py
@@ -0,0 +1,60 @@
+# car0 = {'model': 'lamborghini Aventador LB834',
+#         'price': '420.000',
+#         'year': 2011,
+#         'colour': 'yellow',
+#         'km': 3000
+        
+#         }
+# car1 = {'model': 'Bugatti Chiron',
+#         'price': 3_000_000,
+#         'year': 2018,
+#         'colour': 'blue',
+#         'km': 25
+#         }
+# car2 = {'model': 'Ferrari SF90 Spider',
+#         'price': 57_000,
+#         'year': 2020,
+#         'colour': 'red',
+#         'km': 2500
+#         }
+# # car = car0
+# # print(f"{car['model'].title()}, "
+# #       f"{car['colour']} colour, "
+# #       f"and it's price is {car['price']}$. "
+      
+# #       )
+# # car = car1
+# # print(f"{car['model'].title()}, "
+# #       f"{car['colour']} colour, "
+# #       f"and it's price is {car['price']}$. "
+      
+# #       )
+# # car = car2
+# # print(f"{car['model'].title()}, "
+# #       f"{car['colour']} colour, "
+# #       f"and it's price is {car['price']}$. "
+      
+# #       )#too long right? simpler version is below.Using nesting.
+
+# cars = [car0, car1, car2]
+# for car in cars:
+#     print(f"The model of the car is {car['model'].title()}, "
+#           f"it is in {car['colour']} colour, "
+#           f"and its price is {car['price']}"
+#           )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
